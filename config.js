8af0730b0ff0957d114013d4fc1f2e4ac270fa85
TARUNYADAV1808 <yadavtarun1808@gmail.com>
2026-09-22 22:29:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  SUPABASE_URL: "https://cgxmugnlmwwuzcsshxik.supabase.co"
+  SUPABASE_URL: "https://cgxmugnlmwwuzcsshxik.supabase.co",
   SUPABASE_ANON_KEY:"sb_publishable_QFBjDnOkJyvCe6TRWkjAoQ_pEoH6E7i"
 };
